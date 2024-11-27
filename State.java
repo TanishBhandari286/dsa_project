de0d89fe19aef3accda5c0caeebe6cac2ede7a52
PinkiSharma2005 <23csu238@ncuindia.edu>
2024-11-27 12:28:04 +0530

    Add files via upload

---
+++ State.java
@@ -0,0 +1,14 @@
+package Project;
+
+public class State {
+    String stateName;
+    int distanceFromDelhi;
+    int averageTime;
+
+    public State(String stateName, int distanceFromDelhi, int averageTime) {
+        this.stateName = stateName;
+        this.distanceFromDelhi = distanceFromDelhi;
+        this.averageTime = averageTime;
+    }
+}
+
